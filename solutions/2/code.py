c8d7987e6034d910adefb59706862fb525bd3215
Emil Christensen <emil@prefect.io>
2022-12-02 13:58:08 -0500

    Refactors day 2 part 1

--- solutions/2/code.py
+++ solutions/2/code.py
@@ -1,45 +1,40 @@
 import typer
 
+POINTS_MAP = {
+    "X": 1,  # Rock
+    "Y": 2,  # Paper
+    "Z": 3,  # Scissor
+}
+
+OUTCOMES_MAP = {
+    "X": {
+        "C": "win",
+        "B": "lose",
+        "A": "draw",
+    },
+    "Y": {
+        "A": "win",
+        "C": "lose",
+        "B": "draw",
+    },
+    "Z": {
+        "B": "win",
+        "A": "lose",
+        "C": "draw",
+    },
+}
+
 
 def calculate_score(opp_shape, my_shape):
-    points_map = {
-        "A": 1, # Rock
-        "B": 2, # Paper
-        "C": 3, # Scissor
-        "X": 1, # Rock
-        "Y": 2, # Paper
-        "Z": 3, # Scissor
-    }
-    my_points = points_map[my_shape]
-    opp_points = points_map[opp_shape]
+    shape_points = POINTS_MAP[my_shape]
 
-    # Draw
-    if my_points == opp_points:
-        return my_points + 3
-
-    # Rock
-    if my_shape == "X":
-        # Beats scissor
-        if opp_shape == "C":
-            return my_points + 6
-        # Loses to Paper
-        return my_points
-
-    # Paper
-    if my_shape == "Y":
-        # Beats rock
-        if opp_shape == "A":
-            return my_points + 6
-        # Loses to scissor
-        return my_points
-
-    # Scissor
-    if my_shape == "Z":
-        # Beats paper
-        if opp_shape == "B":
-            return my_points + 6
-        # Loses to rock
-        return my_points
+    outcome = OUTCOMES_MAP[my_shape][opp_shape]
+    if outcome == "win":
+        return shape_points + 6
+    elif outcome == "lose":
+        return shape_points + 3
+    elif outcome == "draw":
+        return shape_points
 
 
 def main(path: str):
@@ -51,13 +46,15 @@ def main(path: str):
             round += 1
             line = fh.readline()
 
-            if line == '':
+            if line == "":
                 break
 
             opp_shape, my_shape = line.strip().split(" ")
             round_score = calculate_score(opp_shape, my_shape)
             total_score += round_score
-            print(f"[Round {round}] {opp_shape} vs {my_shape} - round:{round_score} total:{total_score}")
+            print(
+                f"[Round {round}] {opp_shape} vs {my_shape} - round:{round_score} total:{total_score}"
+            )
 
 
 if __name__ == "__main__":
