d47cdfb8c101292ca761f5e03392e148709535b4
Emil Christensen <emil@prefect.io>
2022-12-04 19:43:42 -0500

    Updates code template and adds shell script to bootstrap new solution

--- template.py
+++ template.py
@@ -2,7 +2,20 @@ import typer
 
 
 def main(path: str):
-    pass
+    total = 0
+    idx = 0
+
+    with open(path, "r") as fh:
+        while True:
+            idx += 1
+            line = fh.readline().strip()
+
+            # Exit condition
+            if line == "":
+                break
+
+            # Code goes here
+            print(f"[{idx}] '{line}' - total: {total}")
 
 
 if __name__ == "__main__":
