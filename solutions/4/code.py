14705cee6739d70cb16d14eb193a1c353e0d35fe
Emil Christensen <emil@prefect.io>
2022-12-04 19:27:14 -0500

    Updates day 4 for part 2

--- solutions/4/code.py
+++ solutions/4/code.py
@@ -1,7 +1,20 @@
 import typer
 
 
-def main(path: str):
+def contains(s1: int, e1: int, s2: int, e2: int):
+    if (s1 >= s2 and e1 <= e2) or (s2 >= s1 and e2 <= e1):
+        return 1
+    return 0
+
+
+def intersects(s1: int, e1: int, s2: int, e2: int):
+    intersection = set(range(s1, e1 + 1)).intersection(set(range(s2, e2 + 1)))
+    if len(intersection) > 0:
+        return 1
+    return 0
+
+
+def main(path: str, strategy: str):
     total = 0
     loop = 0
 
@@ -17,9 +30,14 @@ def main(path: str):
             s1, e1 = (int(i) for i in r1.split("-"))
             s2, e2 = (int(i) for i in r2.split("-"))
 
-            if (s1 >= s2 and e1 <= e2) or (s2 >= s1 and e2 <= e1):
-                total += 1
-                print(f"[{loop}] Ranges {line} overlap... total: {total}")
+            if strategy == "contains":
+                total += contains(s1, e1, s2, e2)
+            elif strategy == "intersects":
+                total += intersects(s1, e1, s2, e2)
+            else:
+                raise ValueError(f"Invalid strategy {strategy}")
+
+            print(f"[{loop}] Ranges {line} {strategy}... total: {total}")
 
 
 if __name__ == "__main__":
